ea8d3abdb53c55a99867b3f509e13dc21d371cb9
sadness_ojisan <sadness.ojisan@gmail.com>
2020-05-20 05:29:15 +0900

    add knobs

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,11 @@
 module.exports = {
-  stories: ['../stories/**/*.stories.js'],
-  addons: ['@storybook/addon-actions', '@storybook/addon-links'],
-  webpackFinal: async config => {
+  stories: ["../stories/**/*.stories.js"],
+  addons: [
+    "@storybook/addon-actions",
+    "@storybook/addon-links",
+    "@storybook/addon-knobs/register",
+  ],
+  webpackFinal: async (config) => {
     // do mutation to the config
 
     return config;
--- stories/1-Button.stories.js
+++ stories/1-Button.stories.js
@@ -1,23 +1,28 @@
-import React from 'react';
+import React from "react";
 
-import { action } from '@storybook/addon-actions';
-import { Button } from '@storybook/react/demo';
+import { withKnobs, text, boolean, number } from "@storybook/addon-knobs";
+import { action } from "@storybook/addon-actions";
+import { Button } from "@storybook/react/demo";
 
 export default {
-  title: 'Button',
-  component: Button,
+  title: "Storybook Knobs",
+  decorators: [withKnobs],
 };
+// Add the `withKnobs` decorator to add knobs support to your stories.
+// You can also configure `withKnobs` as a global decorator.
 
-export const Text = () => <Button onClick={action('clicked')}>Hello Button</Button>;
-
-export const Emoji = () => (
-  <Button onClick={action('clicked')}>
-    <span role="img" aria-label="so cool">
-      😀 😎 👍 💯
-    </span>
+// Knobs for React props
+export const withAButton = () => (
+  <Button disabled={boolean("Disabled", false)} onClick={action("clicked")}>
+    {text("Label", "Hello Storybook")}
   </Button>
 );
 
-Emoji.story = {
-  name: 'with emoji',
+// Knobs as dynamic variables.
+export const asDynamicVariables = () => {
+  const name = text("Name", "James");
+  const age = number("Age", 35);
+  const content = `I am ${name} and I'm ${age} years old.`;
+
+  return <div>{content}</div>;
 };
