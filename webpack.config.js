4baf85d695d17c3a6867ba4070207209d23e6efa
sadness_ojisan <sadness.ojisan@gmail.com>
2020-05-20 05:21:16 +0900

    success build

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: process.env.NODE_ENV,
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "build.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        use: [
+          {
+            loader: "ts-loader",
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js", ".tsx"],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })],
+};
